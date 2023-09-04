dd8325956824cd57487aec8277851ee14ca2a971
Michael Heap <m@michaelheap.com>
2023-09-04 12:34:13 +0100

    Fix CLI parameter order

--- bin.js
+++ bin.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 const run = require(".");
-const args = process.argv.slice(2).reverse();
+const args = process.argv.slice(2);
 
 if (args.length > 2) {
-  throw new Error("Usage: convert-action-to-node [filename] [compiled.js]");
+  throw new Error(
+    "Usage: convert-action-to-node [compiled.js] [/path/to/action.yaml]"
+  );
 }
 run.apply(null, args);
